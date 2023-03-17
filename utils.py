16053c8baa2ace2666e6ae33e2856e7428e5f024
wangzican <zcabanj@ucl.ac.uk>
2023-03-17 18:10:10 +0000

    loading prompts from new data

--- utils.py
+++ utils.py
@@ -1,6 +1,19 @@
 from datasets import load_dataset
 
 
+def load_perc_prompts(num):
+    dataset = load_dataset('csv', data_files={'train': './Baseline/Data/PERC.csv'})
+    joy_dataset = dataset.filter(lambda row: row['Emotion'] == 'surprise' 
+                                 or row['Emotion'] == 'joy' 
+                                 or row['Emotion'] == 'love' 
+                                 or row['Emotion'] == 'peace' 
+                                 or row['Emotion'] == 'courage')
+        
+    prompts = joy_dataset.map(lambda poem: 
+        {'prompt': ' '.join(poem['Poem'].split()[:num])}
+    )['prompt']
+    return prompts
+
 def load_prompts(num):
     dataset = load_dataset(
         "Ozziey/poems_dataset", 
