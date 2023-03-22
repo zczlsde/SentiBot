2bd98bb9e97ef27156af8440265a420d77b8ff2c
Wang Zican Robert <zcabanj@ucl.ac.uk>
2023-03-22 00:46:21 +0000

    load evaluation prompt and grammar checker

--- Baseline/joy_data_loader.py
+++ Baseline/joy_data_loader.py
@@ -1,11 +1,12 @@
 import pandas as pd
 from datasets import load_dataset
+import numpy as np
 import os
 """
 Used for loading positive data to file
 """
 
-if __name__ == "__main__":
+def load_ozzie():
     # ozziey data
     ozziey_path = "./Data/joy_data"
     if not os.path.exists(ozziey_path):
@@ -16,6 +17,8 @@ if __name__ == "__main__":
         joy_dataset = dataset.filter(lambda row: row['label'] == 'joy')
         joy_dataset.save_to_disk(ozziey_path)
 
+
+def load_PERC():
     #PERC data
     PERC_path = "./Data/PERC.csv"
     PERC_xlsx_path = "./Data/PERC_mendelly.xlsx"
@@ -34,3 +37,26 @@ if __name__ == "__main__":
                             header=True)
         else:
             raise ValueError("Incorrect path for xlsx")
+
+def load_evaluation_prompts(length=5, seed=19019509):
+    """
+    load one prompt from each emotion
+    length:  length of the prompt
+    """
+    PERC_path = "./Data/PERC.csv"
+    rng = np.random.default_rng(seed)
+    dataset = pd.read_csv(PERC_path)
+    Emotions = list(set(dataset['Emotion']))
+    Emotions.sort()
+    Prompts = {}
+    for emotion in Emotions:
+        mask = dataset["Emotion"] == emotion
+        prompt = rng.choice(dataset[mask])[0]
+        Prompts[emotion] = prompt.split()[:length]
+    return Prompts
+
+if __name__ == "__main__":
+    # load_ozzie()
+    # load_PERC()
+    p = load_evaluation_prompts(5, 19019509)
+    print(p)
