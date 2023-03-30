d307c0dbb9fd56355289c0a34b276712215a5c6c
wangzican <zcabanj@ucl.ac.uk>
2023-03-30 15:58:56 +0100

    diversity for baseline

--- Baseline/testing.py
+++ Baseline/testing.py
@@ -1,17 +1,15 @@
 from itertools import compress
 import language_tool_python
+import numpy as np
+import torch
+from scipy.spatial.distance import cosine
+from transformers import AutoModel, AutoTokenizer
 
-
-def fluency(verbose = 0):
+def _read_text(path):
     """
-    Unfinished
-    Give a score on the average fluency and fluency distribution
-
-    Returns:
-            error rate: single float
-            error rates: array of float
+    read in the generated text and remove excess
     """
-    with open("./Data/Generations/perc_generations.txt", encoding="utf8") as f:
+    with open(path, encoding="utf8") as f:
         lines = f.readlines()
 
     phrases = []
@@ -22,10 +20,22 @@ def fluency(verbose = 0):
             phrase = ""
             continue
         phrase = phrase + x
-    print(len(phrases))
+    return phrases
+
+def fluency(path = "./Data/Generations/perc_generations.txt", verbose = 0):
+    """
+    Give a score on the average fluency and fluency distribution
+
+    Returns:
+            error rate: single float over all phrases
+            error rates: array of float for each phrase
+    """
+    phrases = _read_text(path)
+    print(len(phrases), " phrases")
 
     # mask = [not x.startswith("===") for x in lines]
     # sentences = list(compress(lines, mask))   
+    # check grammar error rate from the language tool
     tool = language_tool_python.LanguageTool("en-US")
     total_length = 0
     total_errors = 0
@@ -33,20 +43,55 @@ def fluency(verbose = 0):
     error_rates = []
     counter = 0
     for phrase in phrases:
-        if verbose >= 1:
-            counter += 1
-            print(counter)
-        length = len(phrase.split())
-        total_length += length
-        matches = tool.check(phrase)
-        naive_errors = len(matches)
-        total_errors += naive_errors
-        if length > 0:
-            error_rates.append(1-naive_errors/length)
+        try:
+            if verbose >= 1:
+                counter += 1
+                print(counter)
+            length = len(phrase.split())
+            total_length += length
+            matches = tool.check(phrase)
+            naive_errors = len(matches)
+            total_errors += naive_errors
+            if length > 0:
+                error_rates.append(1-naive_errors/length)
+        except KeyboardInterrupt:
+            break
     tool.close()
     error_rate = 1-total_errors/total_length
     return error_rate, error_rates
 
+
+def diversity(path = "./Data/Generations/perc_generations.txt", seed=19019509):
+    """
+    Diversity in 20 phrases
+    """
+    tokenizer = AutoTokenizer.from_pretrained("princeton-nlp/sup-simcse-bert-base-uncased")
+    model = AutoModel.from_pretrained("princeton-nlp/sup-simcse-bert-base-uncased")
+
+    # Tokenize input texts
+    texts = _read_text(path=path)
+    rng = np.random.default_rng(seed=seed)
+    index = rng.choice(len(texts))
+    texts = texts[index:index+20]
+
+    inputs = tokenizer(texts, padding=True, truncation=True, return_tensors="pt")
+
+    # Get the embeddings
+    with torch.no_grad():
+        embeddings = model(**inputs, output_hidden_states=True, return_dict=True).pooler_output
+
+
+    print(embeddings.shape)
+    # Calculate cosine similarities
+    # Cosine similarities are in [-1, 1]. Higher means more similar
+    cosine_sim_0_1 = 1 - cosine(embeddings[0], embeddings[1])
+    cosine_sim_0_2 = 1 - cosine(embeddings[0], embeddings[2])
+
+
+    print("Cosine similarity between \"%s\" and \"%s\" is: %.3f" % (texts[0], texts[1], cosine_sim_0_1))
+    print("Cosine similarity between \"%s\" and \"%s\" is: %.3f" % (texts[0], texts[2], cosine_sim_0_2))
+
 if __name__ == "__main__":
-    rate, rates = fluency(verbose=1)
-    print(rate, rates)
+    # rate, rates = fluency(verbose=1)
+    # print(rate, rates)
+    diversity()
